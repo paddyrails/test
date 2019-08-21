0ecc8d7f9e127f50f31e18de118b28fbe8177d85
paddyrails <p_pillai3@hotmail.com>
2019-08-21 12:10:26 -0400

    react

---
+++ build/precache-manifest.23dc1a21c7e9da717dfb8a2b734f3815.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6d7aea4252cef4bdf93998b908e5067",
+    "url": "/index.html"
+  },
+  {
+    "revision": "961835934257901bac00",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "a8e2f0737c47cade0613",
+    "url": "/static/js/2.d77323ae.chunk.js"
+  },
+  {
+    "revision": "961835934257901bac00",
+    "url": "/static/js/main.17ffd5ab.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
